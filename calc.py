639e02ff4a5b728453f76ab3021d2740de368c4e
SS-CC-YY <songchen@oregonstate.edu>
2021-04-16 08:52:51 +0800

    make some changes in calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,23 @@
-def calc(a,b):
-    sum = a + b
-    #print(sum)
-    diff = a - b
-    mul = a * b
-    div = a / b
+import string
+
+def calc_sum(a,b):
+    print (a+b)
+
+def calc_diff(a,b):
+    print (a-b)
+
+def calc_mul(a,b):
+    print (a*b)
+
+def calc_div(a,b):
+    print (a/b)
+
+operator = {'+':calc_sum,'-':calc_diff,'*':calc_mul,'/':calc_div}
+
+def calc(a,o,b):
+    operator.get(o)(a,b)
+
+a = int (input("Please enter the first number: "))
+b = int (input("Please enter the second number: "))
+o = input("Please enter the operator: ")
+calc(a,o,b)
