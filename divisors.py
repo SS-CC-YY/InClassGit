df972e9edaec2d3360d3003ba8b635191fc708c4
SS-CC-YY <songchen@oregonstate.edu>
2021-04-16 08:38:51 +0800

    check the divisor and print in divisors.py

--- divisors.py
+++ divisors.py
@@ -5,6 +5,17 @@ def number():
     except:
         print("Invalid input!")
 
+def check_divisor(dividend,divisor):
+    if dividend % divisor == 0:
+        return True
+    else:
+        return False
+
+
 num = number()
-for i in range(num):
-    print(i)
+for i in range(num+1):
+    if i ==0 :
+        continue
+    if check_divisor(num,i) == True:
+        print(i)
+    
